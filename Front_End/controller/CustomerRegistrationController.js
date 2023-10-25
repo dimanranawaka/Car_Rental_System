a1e361ec6a986196d4d51c4ffa1b443f53cd5950
dimanranawaka <dimanranawaka@gmail.com>
2023-10-25 16:36:38 +0530

    Front_End : CustomerRegistrationController.js

--- Front_End/controller/CustomerRegistrationController.js
+++ Front_End/controller/CustomerRegistrationController.js
@@ -12,8 +12,8 @@ $("#btnSaveCustomer").on("click",function () {
         contact :$("#cusContact").val(),
         email :$("#cusEmail").val(),
         user:{
-            username:$("#cusUsername"),
-            password:$("#cusPassword"),
+            username:$("#cusUsername").val(),
+            password:$("#cusPassword").val(),
         }
     }
 
@@ -36,7 +36,7 @@ $("#btnSaveCustomer").on("click",function () {
     }
 
     $.ajax({
-        url: baseUrl + "customer ? image",
+        url: baseUrl + "customer?image",
         method: "post",
         async: false,
         data: data,
@@ -62,6 +62,59 @@ const cusNameRegEx = /^[A-z ]{5,20}$/;
 const cusEmailRegEx = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-]+)(\.[a-zA-Z]{2,5}){1,2}$/;
 const cusNicRegEx = /^[0-9]{9,10}[A-z]?$/;
 const cusAddressRegEx = /^[0-9/A-z. ,]{5,}$/;
+const cusPasswordRegEx = /^[0-9/A-z. ,]{5,}$/;
 const cusContactRegEx = /^[0-9]{10}$/;
-const cusSalaryRegEx = /^[0-9]{1,}[.]?[0-9]{1,2}$/;
 
+
+// Let's put those Reg-Ex to a array
+
+let customerValidations = [];
+
+customerValidations.push({
+    reg: cusNameRegEx,
+    field : $('#cusName'),
+    error : 'Customer Name Pattern is Invalid ! : A-z 5-20'
+});
+
+
+customerValidations.push({
+    reg: cusEmailRegEx,
+    field: $('#cusEmail'),
+    error: 'Customer E-mail Pattern is Invalid ! : dimanranawaka@gmail.com'
+});
+
+customerValidations.push({
+    reg:cusNicRegEx,
+    field:$('#cusNic'),
+    error: 'Customer NIC Pattern is Invalid ! : 970790448'
+});
+
+customerValidations.push({
+    reg:cusAddressRegEx,
+    field:$('#cusAddress'),
+    error:'Customer Address Pattern is Invalid ! : A-z 0-9 ,/'
+});
+
+customerValidations.push({
+    reg:cusContactRegEx,
+    field:$('#cusLicense'),
+    error: 'Customer License Pattern is Invalid ! : 12345678'
+});
+
+customerValidations.push({
+    reg:cusContactRegEx,
+    field:$('#cusContact'),
+    error: 'Customer Contact Pattern is Invalid ! : 077123456'
+});
+
+customerValidations.push({
+    reg:cusPasswordRegEx,
+    field:$('#cusPassword'),
+    error: 'Customer Password Pattern is Invalid ! : 077123456'
+});
+
+customerValidations.push({
+    reg:cusPasswordRegEx,
+    field:$('#cusRe-password'),
+    error: 'Customer Re-Password Pattern is Invalid ! : 077123456'
+});
