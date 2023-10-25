8618d932ff5cd290e3881d35f3cd5c82a7649082
dimanranawaka <dimanranawaka@gmail.com>
2023-10-25 10:44:34 +0530

    Front_End : CustomerRegistrationController.js

---
+++ Front_End/controller/CustomerRegistrationController.js
@@ -0,0 +1,32 @@
+/** Save Customer method */
+
+$("#btnSaveCustomer").on("click",function () {
+
+    let data = new FormData($("#customerForm")[0]);
+
+    let json : = {
+        nic: $("#cusNic").val(),
+        name :$("#cusName").val(),
+        license :$("#cusLicense").val(),
+        address :$("#cusAddress").val(),
+        contact :$("#cusContact").val(),
+        email :$("#cusEmail").val(),
+        user:{
+            username:$("#cusUsername"),
+            password:$("#cusPassword"),
+        }
+    }
+
+    $.ajax({
+        url: baseUrl + "customer",
+        async: false,
+        cache: false,
+        data: JSON.stringify(json),
+        contentType: "application/json",
+        dataType: "json",
+        success: function (res) {
+
+        }
+    })
+
+})
