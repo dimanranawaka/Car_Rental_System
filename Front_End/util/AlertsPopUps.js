e188bed36e5382de495dca0f1330aa17b2f67159
dimanranawaka <dimanranawaka@gmail.com>
2023-10-25 10:54:27 +0530

    Front_End : AlertsPopUps.js

---
+++ Front_End/util/AlertsPopUps.js
@@ -0,0 +1,15 @@
+
+/** Saving Alert */
+
+function saveAlert() {
+
+    Swal.fire({
+        position: 'top-end',
+        icon:'success',
+        title:'Successfully Saved !',
+        showConfirmButton: false,
+        timer: 1500
+
+    });
+
+}
