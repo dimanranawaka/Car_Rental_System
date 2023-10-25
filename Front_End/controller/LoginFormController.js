edcddb429924b716e2e5f666a4edd649099fd23c
dimanranawaka <dimanranawaka@gmail.com>
2023-10-25 09:48:44 +0530

    Front_End : LoginFormController.js

--- Front_End/controller/LoginFormController.js
+++ Front_End/controller/LoginFormController.js
@@ -1,10 +1,33 @@
+import {Alert} from "../assets/js/bootstrap.esm";
+
 $("#btnLogin").on("click",function () {
+
     $.ajax({
         url:baseUrl+"login",
         method:"post",
         data: $("#loginForm").serialize(),
+
         success:function (res) {
-            
+
+            switch (res.data.role) {
+
+                case "Admin":
+                    window.open();
+                    break;
+                    
+                case "Customer":
+                    window.open();
+                    break;
+                
+                case "Driver":
+                    window.open();
+                    break;
+                    
+            }
+        },
+        
+        error:function (res) {
+            alert("Invalid Username or Password")
         }
     });
 })
