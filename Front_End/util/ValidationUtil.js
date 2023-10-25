c4a642a06891f84f9fe5fe77039f1bad5a785e21
dimanranawaka <dimanranawaka@gmail.com>
2023-10-26 03:48:39 +0530

    Front_End : ValidationUtil.js

--- Front_End/util/ValidationUtil.js
+++ Front_End/util/ValidationUtil.js
@@ -4,7 +4,7 @@ function checkValidity(validationArray) {
     let errorCount = 0;
     for (let validationArrayElement of validationArray) {
         if (check(validationArrayElement.reg,validationArrayElement.field)){
-
+            textSuccess(validationArrayElement.field, "");
         }else{
             errorCount++;
 
@@ -21,4 +21,18 @@ function check(regex, textField) {
 
 function focusText(textField) {
     textField.focus();
+}
+
+function textSuccess(txtField,error) {
+    if (txtField.val().length <= 0){
+        defaultText(txtField,"");
+    }else{
+        txtField.css("border","1.5px solid green");
+        txtField.parent().children('span').text(error);
+    }
+}
+
+function defaultText (txtField,error){
+    txtField.css("border","1.5px solid #94c3f2");
+    txtField.parent().children('span').text(error);
 }
