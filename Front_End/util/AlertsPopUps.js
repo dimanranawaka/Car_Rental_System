b5a15682ef8e1bb9e0443a7220a973ff82a71015
dimanranawaka <dimanranawaka@gmail.com>
2023-10-26 04:08:52 +0530

    Front_End : AlertsPopUps.js

--- Front_End/util/AlertsPopUps.js
+++ Front_End/util/AlertsPopUps.js
@@ -13,3 +13,48 @@ function saveAlert() {
     });
 
 }
+
+/** Updating Alert */
+
+function updateAlert() {
+
+    Swal.fire({
+        position: 'top-end',
+        icon:'success',
+        title:'Successfully Updated !',
+        showConfirmButton: false,
+        timer: 1500
+
+    });
+
+}
+
+/** Deleting Alert */
+
+function deleteAlert() {
+
+    Swal.fire({
+        position: 'top-end',
+        icon:'success',
+        title:'Successfully Deleted !',
+        showConfirmButton: false,
+        timer: 1500
+
+    });
+
+}
+
+/** Error Alert */
+
+function errorAlert(error) {
+
+    Swal.fire({
+        position: 'top-end',
+        icon:'error',
+        title:error,
+        showConfirmButton: false,
+        timer: 1500
+
+    });
+
+}
