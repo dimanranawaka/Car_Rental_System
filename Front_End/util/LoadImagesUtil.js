65aee651b849ea9e3d7e1008c5a28cde6040a95a
dimanranawaka <dimanranawaka@gmail.com>
2023-10-25 11:44:55 +0530

    Content Updated

--- Front_End/util/LoadImagesUtil.js
+++ Front_End/util/LoadImagesUtil.js
@@ -1,26 +1,21 @@
 /** This one for loading selected images on the site */
 
-function loadImagesOnSite(divId) {
-    $(divId).on("change",function (e) {
+function loadSelectedImage(divId) {
 
+    $(divId).on("change", function (e) {
         let file = e.target.files;
 
-        if (FileReader && file && file.length){
-
+        if (FileReader && file && file.length) {
             let reader = new FileReader();
-
             reader.onload = function () {
-
                 $(divId).parent().children(":eq(0)").css({
-
                     "background": `url(${reader.result})`,
                     "background-size": "cover",
-                    "background-position": "cover"
-
+                    "background-position": "center"
                 });
-
             }
             reader.readAsDataURL(file[0]);
         }
+
     });
 }
