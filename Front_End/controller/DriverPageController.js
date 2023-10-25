87e350122ffe949148a353b8045c86cc0d681088
dimanranawaka <dimanranawaka@gmail.com>
2023-10-26 04:39:47 +0530

    Front_End : DriverPageController.js

--- Front_End/controller/DriverPageController.js
+++ Front_End/controller/DriverPageController.js
@@ -12,3 +12,39 @@ $.ajax({
     }
 });
 
+$.ajax({
+    url: baseUrl + "driver?nic" + currentUser.nic,
+    method:"get",
+    async:false,
+    dataType: "json",
+    contentType: "application/json",
+    success: function (res) {
+        for (let datum of res.data) {
+
+            let rent;
+
+            $.ajax({
+                url: baseUrl + 'rent?rentId=?' + datum.rentId;
+                async: false,
+                method:"get",
+                dataType: "json",
+                contentType:"application/json",
+                success: function (res) {
+                    rent = rent.data;
+                }
+            });
+
+            $("#tblDriverSchedule").append(`
+                <tr>
+                    <td>${datum.rentId}</td>
+                    <td>${rent.pickUpDate.toString().replaceAll("","")}</td>
+                    <td>${rent.pickUpTime.toString().replaceAll("","")}</td>
+                    <td>${rent.x.toString().replaceAll("","")}</td>
+                    <td>${rent.y.toString().replaceAll("","")}</td>
+                    <td>${datum.regNum}</td>
+                </tr>
+            `);
+
+        }
+    }
+})
