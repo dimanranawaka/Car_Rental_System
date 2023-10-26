43f364ae4993e649b18d9a396a4703be2182cd3c
dimanranawaka <dimanranawaka@gmail.com>
2023-10-26 13:05:15 +0530

    Front_End : CarPageController.js

--- Front_End/controller/CarPageController.js
+++ Front_End/controller/CarPageController.js
@@ -1,1 +1,14 @@
 $("#viewCar").fadeIn();
+
+$.ajax({
+    url: baseUrl+ "car",
+    method: "get",
+
+    success:function (res) {
+        loadAllCars(res.data);
+    }
+});
+
+function loadAllCars(cars) {
+
+}
