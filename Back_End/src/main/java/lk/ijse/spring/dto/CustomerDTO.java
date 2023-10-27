2623b657fece09402166d40c40e69141bb711b92
dimanranawaka <dimanranawaka@gmail.com>
2023-10-27 19:24:17 +0530

    Back_End : CustomerDTO.java

---
+++ Back_End/src/main/java/lk/ijse/spring/dto/CustomerDTO.java
@@ -0,0 +1,23 @@
+package lk.ijse.spring.dto;
+
+import lk.ijse.spring.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+public class CustomerDTO {
+    private String nic;
+    private String name;
+    private String license;
+    private String address;
+    private String contact;
+    private String email;
+    private User user;
+    private String nicImage;
+    private String licenseImage;
+}
