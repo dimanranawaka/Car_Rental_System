deedb9a3285a96560f14ed494d326b5d394139de
dimanranawaka <dimanranawaka@gmail.com>
2023-10-27 19:38:25 +0530

    Back_End : DriverDTO.java

--- Back_End/src/main/java/lk/ijse/spring/dto/DriverDTO.java
+++ Back_End/src/main/java/lk/ijse/spring/dto/DriverDTO.java
@@ -1,4 +1,23 @@
 package lk.ijse.spring.dto;
 
+import lk.ijse.spring.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class DriverDTO {
+    private String nic;
+    private String license;
+    private String name;
+    private String address;
+    private String contact;
+    private String email;
+    private String availabilityStatus;
+    private User user;
+    private String licenseImage;
 }
