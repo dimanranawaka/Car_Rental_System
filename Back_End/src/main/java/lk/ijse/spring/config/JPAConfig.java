f60a6e14ca5f48eacdf1a56a94c9d186181733f1
dimanranawaka <dimanranawaka@gmail.com>
2023-10-27 14:21:46 +0530

    Content Updated

---
+++ Back_End/src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -0,0 +1,11 @@
+package lk.ijse.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@PropertySource("classpath:properties.properties")
+public class JPAConfig {
+}
