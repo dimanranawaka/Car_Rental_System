8dde58d3dabbe97db8f4e9eea044426e27e10e07
dimanranawaka <dimanranawaka@gmail.com>
2023-10-27 18:13:22 +0530

    Back_End : Admin.java

--- Back_End/src/main/java/lk/ijse/spring/entity/Admin.java
+++ Back_End/src/main/java/lk/ijse/spring/entity/Admin.java
@@ -1,4 +1,27 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class Admin {
+    @Id
+    private String nic;
+    private String name;
+    private String license;
+    private String address;
+    private String contact;
+    private String email;
+    @OneToOne(cascade = CascadeType.ALL)
+    private User user;
 }
