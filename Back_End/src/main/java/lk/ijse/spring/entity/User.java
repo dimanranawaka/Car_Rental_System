820db942bd7a78f5dd42d1d665260160f61c8747
dimanranawaka <dimanranawaka@gmail.com>
2023-10-27 18:00:20 +0530

    Back_End : User.java

--- Back_End/src/main/java/lk/ijse/spring/entity/User.java
+++ Back_End/src/main/java/lk/ijse/spring/entity/User.java
@@ -1,4 +1,20 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class User {
+    @Id
+    private String username;
+    private String password;
+    private String role;
 }
