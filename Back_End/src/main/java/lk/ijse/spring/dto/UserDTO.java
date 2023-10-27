73caf3de00ba9da17de70c120cdf364330cdf137
dimanranawaka <dimanranawaka@gmail.com>
2023-10-27 19:28:59 +0530

    Back_End : UserDTO.java

--- Back_End/src/main/java/lk/ijse/spring/dto/UserDTO.java
+++ Back_End/src/main/java/lk/ijse/spring/dto/UserDTO.java
@@ -1,4 +1,16 @@
 package lk.ijse.spring.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class UserDTO {
+    private String username;
+    private String password;
+    private String role;
 }
