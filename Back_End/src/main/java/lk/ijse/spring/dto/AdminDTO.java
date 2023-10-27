9cf7a493d2c22fb91812b647d2d3540a3a04ba68
dimanranawaka <dimanranawaka@gmail.com>
2023-10-27 19:33:17 +0530

    Back_End : AdminDTO.java

--- Back_End/src/main/java/lk/ijse/spring/dto/AdminDTO.java
+++ Back_End/src/main/java/lk/ijse/spring/dto/AdminDTO.java
@@ -1,4 +1,21 @@
 package lk.ijse.spring.dto;
 
+import lk.ijse.spring.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class AdminDTO {
+    private String nic;
+    private String name;
+    private String license;
+    private String address;
+    private String contact;
+    private String email;
+    private User user;
 }
