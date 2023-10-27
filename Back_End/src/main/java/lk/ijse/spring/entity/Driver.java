c5c52dd9620953f6ac4a321221a9f6fd74d331a8
dimanranawaka <dimanranawaka@gmail.com>
2023-10-27 18:21:33 +0530

    Content Updated

--- Back_End/src/main/java/lk/ijse/spring/entity/Driver.java
+++ Back_End/src/main/java/lk/ijse/spring/entity/Driver.java
@@ -1,4 +1,30 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class Driver {
+    @Id
+    private String nic;
+    private String license;
+    private String name;
+    private String address;
+    private String contact;
+    private String email;
+    private String availabilityStatus;
+    @OneToOne(cascade = CascadeType.ALL)
+    private User user;
+    private String licenseImage;
 }
