28a74c27fc899085036a64f64ea0a6ba6651c7b6
dimanranawaka <dimanranawaka@gmail.com>
2023-10-27 17:50:12 +0530

    Back_End : Customer.java

--- Back_End/src/main/java/lk/ijse/spring/entity/Customer.java
+++ Back_End/src/main/java/lk/ijse/spring/entity/Customer.java
@@ -1,5 +1,25 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class Customer {
+    @Id
+    private String nic;
+    private String name;
+    private String license;
+    private String address;
+    private String contact;
+    private String email;
 
 }
