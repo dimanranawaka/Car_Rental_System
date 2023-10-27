e4793abfb1f5e9ee32564170be31325fc9424f59
dimanranawaka <dimanranawaka@gmail.com>
2023-10-27 17:09:33 +0530

    Back_End : JPAConfig.java

--- Back_End/src/main/java/lk/ijse/spring/config/JPAConfig.java
+++ Back_End/src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -1,11 +1,47 @@
 package lk.ijse.spring.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableTransactionManagement
 @PropertySource("classpath:properties.properties")
 public class JPAConfig {
+
+    @Autowired
+    Environment environment;
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean(){
+
+        // This is the Spring Data JPA main object which handles all the features
+
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+
+        factory.setDataSource(ds);
+
+        factory.setJpaVendorAdapter(vad);
+
+        factory.setPackagesToScan(environment.getRequiredProperty("properties.entity"));
+
+        return factory;
+    }
+
+    @Bean
+    public DataSource dataSource(){
+
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+
+
+
+    }
+
 }
