63b484e05c7e07b26b70d83494e116ef86ec2663
dimanranawaka <dimanranawaka@gmail.com>
2023-10-28 13:45:00 +0530

    Back_End : CarDTO.java

--- Back_End/src/main/java/lk/ijse/spring/dto/CarDTO.java
+++ Back_End/src/main/java/lk/ijse/spring/dto/CarDTO.java
@@ -1,4 +1,32 @@
 package lk.ijse.spring.dto;
 
+import lk.ijse.spring.embeddable.CarImage;
+import lk.ijse.spring.embeddable.FreeMileage;
+import lk.ijse.spring.embeddable.Price;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class CarDTO {
+    private String regNum;
+    private String type;
+    private String color;
+    private String brand;
+    private String Availability;
+    private String transmissionType;
+    private String fuelType;
+    private int passengers;
+    private Price price;
+    private FreeMileage freeMileage;
+    private BigDecimal extraKMPrice;
+    private BigDecimal lostDamageCost;
+    private String meterValue;
+    private CarImage carImage;
 }
