991e32f5c6471d16116b7a8243c4343306018bf5
dimanranawaka <dimanranawaka@gmail.com>
2023-10-28 13:52:47 +0530

    Back_End : RentDetailsDTO.java

--- Back_End/src/main/java/lk/ijse/spring/dto/RentDetailsDTO.java
+++ Back_End/src/main/java/lk/ijse/spring/dto/RentDetailsDTO.java
@@ -1,4 +1,25 @@
 package lk.ijse.spring.dto;
 
+import lk.ijse.spring.entity.Car;
+import lk.ijse.spring.entity.Driver;
+import lk.ijse.spring.entity.Rent;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class RentDetailsDTO {
+    private String rentId;
+    private String regNum;
+    private String nic;
+    private BigDecimal driverCost;
+    private BigDecimal carCost;
+    private Rent rent;
+    private Car car;
+    private Driver driver;
 }
