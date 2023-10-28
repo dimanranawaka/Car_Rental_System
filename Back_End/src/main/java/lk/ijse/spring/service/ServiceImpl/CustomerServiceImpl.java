7787549cbdb5a258818958a8174a8dd94eab2d63
dimanranawaka <dimanranawaka@gmail.com>
2023-10-28 16:12:53 +0530

    Back_End : CustomerServiceImpl.java

--- Back_End/src/main/java/lk/ijse/spring/service/ServiceImpl/CustomerServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/spring/service/ServiceImpl/CustomerServiceImpl.java
@@ -1,13 +1,23 @@
 package lk.ijse.spring.service.ServiceImpl;
 
 import lk.ijse.spring.dto.CustomerDTO;
+import lk.ijse.spring.repo.CustomerRepo;
 import lk.ijse.spring.service.CustomerService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional // AOP //
 public class CustomerServiceImpl implements CustomerService {
+
+    @Autowired
+    CustomerRepo customerRepo;
+
+    @Autowired
+    ModelMapper modelMapper;
+
     public CustomerServiceImpl() {
         System.out.println("CustomerServiceImpl : Instantiated");
     }
