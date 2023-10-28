64f42330b45c625de514475d03bb4cb03a510b06
dimanranawaka <dimanranawaka@gmail.com>
2023-10-28 16:42:17 +0530

    Back_End : CustomerController.java

--- Back_End/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ Back_End/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -1,15 +1,22 @@
 package lk.ijse.spring.controller;
 
+import lk.ijse.spring.dto.CustomerDTO;
+import lk.ijse.spring.service.CustomerService;
 import lk.ijse.spring.util.ResponseUtil;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/customer")
+@Transactional
+@CrossOrigin
 public class CustomerController {
+    @Autowired
+    CustomerService customerService;
     @PostMapping
-    public ResponseUtil addCustomer(){
+    public ResponseUtil addCustomer(@RequestBody CustomerDTO customerDTO){
+        customerService.addCustomer(customerDTO);
         return new ResponseUtil("Ok","Successfully Added!","");
     }
 }
