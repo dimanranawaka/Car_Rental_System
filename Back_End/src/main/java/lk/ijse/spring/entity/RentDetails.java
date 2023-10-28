5589581c202f9f6114baf44df4f88df061d5cf01
dimanranawaka <dimanranawaka@gmail.com>
2023-10-28 11:13:01 +0530

    Content Updated

--- Back_End/src/main/java/lk/ijse/spring/entity/RentDetails.java
+++ Back_End/src/main/java/lk/ijse/spring/entity/RentDetails.java
@@ -5,16 +5,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.math.BigDecimal;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @ToString
+@IdClass(CarRent_PK.class)
 public class RentDetails {
     @Id
     private String rentId;
