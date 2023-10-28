b2683cb8ff00259e0bae6deac4225087b4e49c3b
dimanranawaka <dimanranawaka@gmail.com>
2023-10-28 15:18:56 +0530

    Back_End : CustomerController.java

--- Back_End/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ Back_End/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -1,9 +1,15 @@
 package lk.ijse.spring.controller;
 
+import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
+    @PostMapping
+    public ResponseUtil addCustomer(){
+        return new ResponseUtil("Ok","Successfully Added!","");
+    }
 }
