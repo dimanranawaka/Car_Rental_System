5691627f94026215f4ae89067f37e9052107902b
dimanranawaka <dimanranawaka@gmail.com>
2023-10-28 09:57:32 +0530

    Back_End : Car.java

--- Back_End/src/main/java/lk/ijse/spring/entity/Car.java
+++ Back_End/src/main/java/lk/ijse/spring/entity/Car.java
@@ -1,4 +1,30 @@
 package lk.ijse.spring.entity;
 
+import lk.ijse.spring.embeddable.Price;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class Car {
+    @Id
+    private String regNum;
+    private String type;
+    private String color;
+    private String brand;
+    private String availability;
+    private String transmissionType;
+    private String fuelType;
+    private int passengers;
+    @Embedded
+    private Price price;
 }
