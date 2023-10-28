640623f6ba51eb4cd0785df9307c6fc677380313
dimanranawaka <dimanranawaka@gmail.com>
2023-10-28 20:06:12 +0530

    Back_End : PaymentController.java

--- Back_End/src/main/java/lk/ijse/spring/dto/PaymentController.java
+++ Back_End/src/main/java/lk/ijse/spring/dto/PaymentController.java
@@ -1,11 +1,19 @@
 package lk.ijse.spring.dto;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.spring.service.PaymentService;
+import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/payment")
 @CrossOrigin
 public class PaymentController {
+    @Autowired
+    PaymentService service;
+
+    @PostMapping
+    public ResponseUtil addPayment(@RequestBody PaymentDTO dto){
+        return new ResponseUtil("","","");
+    }
 }
