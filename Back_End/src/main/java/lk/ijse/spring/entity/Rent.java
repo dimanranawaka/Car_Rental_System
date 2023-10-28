ef7cbda3d7a764bda5bac0afc67c23ba3137b04c
dimanranawaka <dimanranawaka@gmail.com>
2023-10-28 09:36:22 +0530

    Back_End : Rent.java

--- Back_End/src/main/java/lk/ijse/spring/entity/Rent.java
+++ Back_End/src/main/java/lk/ijse/spring/entity/Rent.java
@@ -1,5 +1,33 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class Rent {
+    @Id
+    private String rentId;
+    @ManyToOne(cascade = {CascadeType.REFRESH,CascadeType.DETACH})
+    @JoinColumn(name = "nic",referencedColumnName = "nic", nullable = false)
+    private Customer nic;
+    private LocalDate pickUpDate;
+    private LocalTime pickUpTime;
+    private LocalDate returnDate;
+    private LocalTime returnTime;
+    private String driverRequest;
+    private String status;
+    private BigDecimal cost;
+    private String description;
 
 }
