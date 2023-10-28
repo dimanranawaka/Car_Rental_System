42bc191f9b9ce81b7a3d98c3b96795310f484b53
dimanranawaka <dimanranawaka@gmail.com>
2023-10-28 09:44:19 +0530

    Back_End : RentDetails.java

--- Back_End/src/main/java/lk/ijse/spring/entity/RentDetails.java
+++ Back_End/src/main/java/lk/ijse/spring/entity/RentDetails.java
@@ -1,5 +1,28 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.math.BigDecimal;
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class RentDetails {
+    @Id
+    private String rentId;
+    private String nic;
+    private BigDecimal driverCost;
+    private BigDecimal carCost;
+    @ManyToOne
+    @JoinColumn(name = "rentId", referencedColumnName = "rentId", insertable = false, updatable = false)
+    private Rent rent;
 
 }
