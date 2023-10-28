5855fa5bdc9e7171f623d12de80e1c6f8a05aff9
dimanranawaka <dimanranawaka@gmail.com>
2023-10-28 11:32:14 +0530

    Back_End : Payment.java

--- Back_End/src/main/java/lk/ijse/spring/entity/Payment.java
+++ Back_End/src/main/java/lk/ijse/spring/entity/Payment.java
@@ -1,4 +1,31 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer paymentId;
+    @ManyToOne(cascade = {CascadeType.REFRESH,CascadeType.DETACH})
+    @JoinColumn(name = "rentId", referencedColumnName = "rentId",nullable = false)
+    Rent rentId;
+    String type;
+    String description;
+    LocalTime date;
+    LocalTime time;
+    BigDecimal total;
+    BigDecimal cash;
+    BigDecimal balance;
 }
