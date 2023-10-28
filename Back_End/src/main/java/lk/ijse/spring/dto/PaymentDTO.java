7a5c2a6deb496c6f7c09884974957e90dee7ce9e
dimanranawaka <dimanranawaka@gmail.com>
2023-10-28 13:42:37 +0530

    Back_End : PaymentDTO.java

--- Back_End/src/main/java/lk/ijse/spring/dto/PaymentDTO.java
+++ Back_End/src/main/java/lk/ijse/spring/dto/PaymentDTO.java
@@ -1,4 +1,27 @@
 package lk.ijse.spring.dto;
 
+import lk.ijse.spring.entity.Rent;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+import java.time.LocalTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class PaymentDTO {
+
+    Integer paymentId;
+    Rent rentId;
+    String type;
+    String description;
+    LocalTime date;
+    LocalTime time;
+    BigDecimal total;
+    BigDecimal cash;
+    BigDecimal balance;
 }
