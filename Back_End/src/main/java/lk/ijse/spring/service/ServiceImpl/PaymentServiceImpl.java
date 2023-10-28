5ced6d017b7338e4f65a078492c6d45686611ba3
dimanranawaka <dimanranawaka@gmail.com>
2023-10-28 20:09:22 +0530

    Content Updated

--- Back_End/src/main/java/lk/ijse/spring/service/ServiceImpl/PaymentServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/spring/service/ServiceImpl/PaymentServiceImpl.java
@@ -1,13 +1,17 @@
 package lk.ijse.spring.service.ServiceImpl;
 
 import lk.ijse.spring.dto.PaymentDTO;
+import lk.ijse.spring.repo.PaymentRepo;
 import lk.ijse.spring.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class PaymentServiceImpl implements PaymentService {
+    @Autowired
+    PaymentRepo paymentRepo;
     @Override
     public PaymentDTO addPayment(PaymentDTO dto) {
         return null;
