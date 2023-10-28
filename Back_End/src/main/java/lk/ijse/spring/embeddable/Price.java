6272b9ffc58d218eae2372fab2fbd46cb33e18e9
dimanranawaka <dimanranawaka@gmail.com>
2023-10-28 09:21:41 +0530

    Back_End : Price.java

--- Back_End/src/main/java/lk/ijse/spring/embeddable/Price.java
+++ Back_End/src/main/java/lk/ijse/spring/embeddable/Price.java
@@ -1,4 +1,18 @@
 package lk.ijse.spring.embeddable;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Embeddable;
+import java.math.BigDecimal;
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class Price {
+    private BigDecimal dailyPriceRate;
+    private BigDecimal monthlyPriceRate;
 }
