2da9beb8d692ee8b6e94ae5a66d523d0d8a1baf2
dimanranawaka <dimanranawaka@gmail.com>
2023-10-28 09:17:22 +0530

    Back_End : FreeMileage.java

--- Back_End/src/main/java/lk/ijse/spring/embeddable/FreeMileage.java
+++ Back_End/src/main/java/lk/ijse/spring/embeddable/FreeMileage.java
@@ -1,4 +1,20 @@
 package lk.ijse.spring.embeddable;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class FreeMileage {
+    private String front;
+    private String back;
+    private String side;
+    private String interior;
 }
