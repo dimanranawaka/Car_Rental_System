c1d94f2b6c5502c6b074fc7089007f14c834f967
dimanranawaka <dimanranawaka@gmail.com>
2023-10-28 16:09:48 +0530

    Content Updated

--- Back_End/src/main/java/lk/ijse/spring/service/CustomerService.java
+++ Back_End/src/main/java/lk/ijse/spring/service/CustomerService.java
@@ -1,4 +1,7 @@
 package lk.ijse.spring.service;
 
+import lk.ijse.spring.dto.CustomerDTO;
+
 public interface CustomerService {
+    void addCustomer(CustomerDTO dto);
 }
--- Back_End/src/main/java/lk/ijse/spring/service/ServiceImpl/CustomerServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/spring/service/ServiceImpl/CustomerServiceImpl.java
@@ -1,4 +1,19 @@
 package lk.ijse.spring.service.ServiceImpl;
 
-public class CustomerServiceImpl {
+import lk.ijse.spring.dto.CustomerDTO;
+import lk.ijse.spring.service.CustomerService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional // AOP //
+public class CustomerServiceImpl implements CustomerService {
+    public CustomerServiceImpl() {
+        System.out.println("CustomerServiceImpl : Instantiated");
+    }
+
+    @Override
+    public void addCustomer(CustomerDTO dto) {
+
+    }
 }
