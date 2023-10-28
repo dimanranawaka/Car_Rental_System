e44d6fbff33008a9a23e59980fd5e2423aff5708
dimanranawaka <dimanranawaka@gmail.com>
2023-10-28 19:48:18 +0530

    Back_End : UserController.java

--- Back_End/src/main/java/lk/ijse/spring/controller/UserController.java
+++ Back_End/src/main/java/lk/ijse/spring/controller/UserController.java
@@ -2,6 +2,7 @@ package lk.ijse.spring.controller;
 
 import lk.ijse.spring.service.UserService;
 import lk.ijse.spring.util.ResponseUtil;
+import lk.ijse.spring.util.UserUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,5 +16,10 @@ public class UserController {
     @PostMapping
     public ResponseUtil getUser(@RequestParam String username, @RequestParam String password){
         return new ResponseUtil("Ok","Successfully Logged-In!",service.getUser(username,password));
+    }
+
+    @GetMapping
+    public ResponseUtil getCurrentUserDetails(){
+        return new ResponseUtil("Ok","User Details Loaded Successfully!", UserUtil.currentUser);
     }
 }
