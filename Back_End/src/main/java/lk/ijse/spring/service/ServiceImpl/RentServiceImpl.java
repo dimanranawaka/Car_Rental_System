bf9338aa391e241676625714def75e2c3fc59ed6
dimanranawaka <dimanranawaka@gmail.com>
2023-10-28 20:30:21 +0530

    Content Updated

--- Back_End/src/main/java/lk/ijse/spring/service/ServiceImpl/RentServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/spring/service/ServiceImpl/RentServiceImpl.java
@@ -1,4 +1,13 @@
 package lk.ijse.spring.service.ServiceImpl;
 
+import lk.ijse.spring.repo.RentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class RentServiceImpl {
+    @Autowired
+    RentRepo rentRepo;
 }
