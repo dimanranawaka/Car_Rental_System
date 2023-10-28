c077911ae460ca93b239ca62c0b48862ac0c6d9e
dimanranawaka <dimanranawaka@gmail.com>
2023-10-28 18:50:59 +0530

    Content Updated

---
+++ Back_End/src/test/java/lk/ijse/spring/repo/CustomerRepoTest.java
@@ -0,0 +1,12 @@
+package lk.ijse.spring.repo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerRepoTest {
+
+    @Test
+    void countCustomersByNic() {
+    }
+}
