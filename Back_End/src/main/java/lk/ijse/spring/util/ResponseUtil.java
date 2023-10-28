2a0eb2474c1eeec08945cceae26173f309cf12cf
dimanranawaka <dimanranawaka@gmail.com>
2023-10-28 14:46:40 +0530

    Content Updated

---
+++ Back_End/src/main/java/lk/ijse/spring/util/ResponseUtil.java
@@ -0,0 +1,16 @@
+package lk.ijse.spring.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+public class ResponseUtil {
+    String state;
+    String message;
+    String data;
+}
