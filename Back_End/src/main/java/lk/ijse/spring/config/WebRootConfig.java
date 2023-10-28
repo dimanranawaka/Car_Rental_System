dd379b0f25d120381cfb0fdebce0a9cdacb2dfdf
dimanranawaka <dimanranawaka@gmail.com>
2023-10-28 15:58:31 +0530

    Content Updated

--- Back_End/src/main/java/lk/ijse/spring/config/WebRootConfig.java
+++ Back_End/src/main/java/lk/ijse/spring/config/WebRootConfig.java
@@ -1,10 +1,12 @@
 package lk.ijse.spring.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import({JPAConfig.class})
+@ComponentScan(basePackages = {"lk.ijse.spring.service"})
 public class WebRootConfig {
     public WebRootConfig() {
         System.out.println("WebRootConfig : Instantiated");
