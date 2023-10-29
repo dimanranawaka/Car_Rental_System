a715929f1c88fd77dead226a7468e4b969d969e8
dimanranawaka <dimanranawaka@gmail.com>
2023-10-29 12:07:55 +0530

    Content Updated

--- Back_End/src/main/java/lk/ijse/spring/service/CarService.java
+++ Back_End/src/main/java/lk/ijse/spring/service/CarService.java
@@ -1,5 +1,6 @@
 package lk.ijse.spring.service;
 
+import lk.ijse.spring.dto.CarAllDTO;
 import lk.ijse.spring.dto.CarDTO;
 import lk.ijse.spring.dto.CarImageDTO;
 import lk.ijse.spring.entity.Car;
@@ -10,4 +11,6 @@ public interface CarService {
     void addCar(CarDTO dto) throws RuntimeException;
 
     List<Car> getAllCars() throws RuntimeException;
+
+    public CarAllDTO getCar(String regNum);
 }
