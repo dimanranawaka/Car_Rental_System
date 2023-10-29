6a16daa6e3a8d57bceaa6e81a5b9038772c11092
dimanranawaka <dimanranawaka@gmail.com>
2023-10-29 09:03:29 +0530

    Content Updated

--- Back_End/src/main/java/lk/ijse/spring/service/ServiceImpl/CarServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/spring/service/ServiceImpl/CarServiceImpl.java
@@ -1,4 +1,9 @@
 package lk.ijse.spring.service.ServiceImpl;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class CarServiceImpl {
 }
