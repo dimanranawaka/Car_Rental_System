cc38bd50e15498d7d6bfb65542497c65bc07951f
dimanranawaka <dimanranawaka@gmail.com>
2023-10-29 09:08:51 +0530

    Content Updated

--- Back_End/src/main/java/lk/ijse/spring/service/ServiceImpl/CarServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/spring/service/ServiceImpl/CarServiceImpl.java
@@ -1,13 +1,24 @@
 package lk.ijse.spring.service.ServiceImpl;
 
 import lk.ijse.spring.dto.CarDTO;
+import lk.ijse.spring.repo.CarRepo;
+import lk.ijse.spring.repo.RentDetailsRepo;
 import lk.ijse.spring.service.CarService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class CarServiceImpl implements CarService {
+    @Autowired
+    CarRepo carRepo;
+    @Autowired
+    ModelMapper modelMapper;
+    @Autowired
+    RentDetailsRepo detailsRepo;
+
     @Override
     public void addCar(CarDTO dto) {
 
