122ee5df6de4246a0728dde98c96bbecb13c3a34
dimanranawaka <dimanranawaka@gmail.com>
2023-10-29 06:57:03 +0530

    Content Updated

--- Back_End/src/main/java/lk/ijse/spring/config/WebAppConfig.java
+++ Back_End/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -1,14 +1,23 @@
 package lk.ijse.spring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = {"lk.ijse.spring.controller","lk.ijse.spring.advisor"})
-public class WebAppConfig {
+public class WebAppConfig implements WebMvcConfigurer {
     public WebAppConfig() {
         System.out.println("WebAppConfig : Instantiated");
+    }
+    @Bean
+    public CommonsMultipartResolver commonsMultipartResolver(){
+        CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
+        commonsMultipartResolver.setMaxUploadSize(10000000);
+        return  commonsMultipartResolver;
     }
 }
