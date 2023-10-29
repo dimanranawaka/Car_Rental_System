cfb26b749f0412749d0684b1b81a4b1947dedde2
dimanranawaka <dimanranawaka@gmail.com>
2023-10-29 09:04:39 +0530

    Content Updated

--- Back_End/src/main/java/lk/ijse/spring/controller/CarController.java
+++ Back_End/src/main/java/lk/ijse/spring/controller/CarController.java
@@ -1,6 +1,8 @@
 package lk.ijse.spring.controller;
 
+import lk.ijse.spring.service.CarService;
 import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class CarController {
+
+    @Autowired
+    CarService service;
 
     @PostMapping
     public ResponseUtil addCar(){
--- Back_End/src/main/java/lk/ijse/spring/service/ServiceImpl/CarServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/spring/service/ServiceImpl/CarServiceImpl.java
@@ -1,9 +1,10 @@
 package lk.ijse.spring.service.ServiceImpl;
 
+import lk.ijse.spring.service.CarService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
-public class CarServiceImpl {
+public class CarServiceImpl implements CarService {
 }
