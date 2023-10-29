a19794c9da48aa0a63fdb4b694ad49f2c5cce152
dimanranawaka <dimanranawaka@gmail.com>
2023-10-29 08:56:28 +0530

    Content Updated

--- Back_End/src/main/java/lk/ijse/spring/util/ImagePathWriterUtil.java
+++ Back_End/src/main/java/lk/ijse/spring/util/ImagePathWriterUtil.java
@@ -1,4 +1,18 @@
 package lk.ijse.spring.util;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class ImagePathWriterUtil {
+    public static String projectPath = "";
+
+    public String imagePathWriter(@org.jetbrains.annotations.NotNull MultipartFile multipartFile, Path pathLocation) throws IOException {
+        Files.write(pathLocation, multipartFile.getBytes());
+        multipartFile.transferTo(pathLocation);
+
+        return pathLocation.toString().replace("","");
+    }
 }
