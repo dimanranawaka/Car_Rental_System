4566c08c01026d3fcf7bc62f3b72d2ad43e1d83c
dimanranawaka <dimanranawaka@gmail.com>
2023-10-30 16:17:00 +0530

    Content Updated

--- Back_End/src/main/java/lk/ijse/spring/service/impl/RentServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/spring/service/impl/RentServiceImpl.java
@@ -1,15 +1,71 @@
 package lk.ijse.spring.service.impl;
 
+import lk.ijse.spring.dto.RentDTO;
+import lk.ijse.spring.entity.Car;
+import lk.ijse.spring.entity.Driver;
+import lk.ijse.spring.entity.Rent;
+import lk.ijse.spring.entity.RentDetails;
+import lk.ijse.spring.repo.CarRepo;
+import lk.ijse.spring.repo.DriverRepo;
+import lk.ijse.spring.repo.RentDetailsRepo;
 import lk.ijse.spring.repo.RentRepo;
 import lk.ijse.spring.service.RentService;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Random;
 
 @Service
 @Transactional
 public class RentServiceImpl implements RentService {
+    
+    @Autowired
+    ModelMapper modelMapper;
+    
     @Autowired
     RentRepo rentRepo;
+    
+    @Autowired
+    DriverRepo driverRepo;
+
+    @Autowired
+    RentDetailsRepo rentDetailsRepo;
+
+    @Autowired
+    CarRepo carRepo;
+
+    @Override
+    public void rentRequest(RentDTO rentDTO) throws RuntimeException {
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        Rent map = modelMapper.map(rentDTO, Rent.class);
+
+        System.out.println(map);
+        if (rentDTO.getDriverRequest().equals("YES")){
+            List<Driver> allAvailableDriverList = driverRepo.getAllAvailableDriverList();
+
+            int i;
+            for (RentDetails rentDetail : map.getRentDetails()) {
+                // Randomly selecting a driver from above List
+                i = new Random().nextInt(allAvailableDriverList.size());
+
+                // Setting Nic for Each driver from the above List
+                rentDetail.setNic(allAvailableDriverList.get(i).getNic());
+                Car car = carRepo.findById(rentDetail.getRegNum()).get();
+                car.setAvailability("NO");
+
+                carRepo.save(car);
+
+                allAvailableDriverList.get(i).setAvailabilityStatus("NO");
+                // Saving Each driver from the above List
+                driverRepo.save(allAvailableDriverList.get(i));
+
+            }
+
+        }
+        rentRepo.save(map);
+    }
 }
