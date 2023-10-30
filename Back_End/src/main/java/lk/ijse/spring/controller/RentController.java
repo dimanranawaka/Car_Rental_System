3fcb17f7812f55d028aa978b33c5c64d49871e14
dimanranawaka <dimanranawaka@gmail.com>
2023-10-30 15:56:56 +0530

    Content Updated

--- Back_End/src/main/java/lk/ijse/spring/controller/RentController.java
+++ Back_End/src/main/java/lk/ijse/spring/controller/RentController.java
@@ -1,8 +1,10 @@
 package lk.ijse.spring.controller;
 
 import lk.ijse.spring.service.RentService;
+import lk.ijse.spring.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class RentController {
     @Autowired
     RentService rentService;
+
+    @PostMapping
+    public ResponseUtil rentRequest(){
+        return new ResponseUtil("Ok","Request Successfully","");
+    }
 }
