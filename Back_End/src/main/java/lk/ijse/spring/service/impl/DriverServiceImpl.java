2ca5cc8bcefe7c68c8b52ef2f7c2b3f74d401ebb
dimanranawaka <dimanranawaka@gmail.com>
2023-10-30 08:28:16 +0530

    Content Updated

--- Back_End/src/main/java/lk/ijse/spring/service/impl/DriverServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/spring/service/impl/DriverServiceImpl.java
@@ -1,6 +1,10 @@
 package lk.ijse.spring.service.impl;
 
+import lk.ijse.spring.dto.DriverDTO;
+import lk.ijse.spring.repo.DriverRepo;
 import lk.ijse.spring.service.DriverService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -8,5 +12,14 @@ import javax.transaction.Transactional;
 @Service
 @Transactional
 public class DriverServiceImpl implements DriverService {
+    @Autowired
+    ModelMapper modelMapper;
 
+    @Autowired
+    DriverRepo driverRepo;
+
+    @Override
+    public void addDriver(DriverDTO dto) {
+
+    }
 }
