96634d1ecb980197e5ea3d0ce307624bfb010fee
dimanranawaka <dimanranawaka@gmail.com>
2023-10-30 08:25:08 +0530

    Content Updated

--- Back_End/src/main/java/lk/ijse/spring/controller/DriverController.java
+++ Back_End/src/main/java/lk/ijse/spring/controller/DriverController.java
@@ -1,13 +1,10 @@
 package lk.ijse.spring.controller;
 
+import lk.ijse.spring.dto.DriverDTO;
 import lk.ijse.spring.service.DriverService;
-import lk.ijse.spring.service.RentService;
 import lk.ijse.spring.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/driver")
@@ -17,7 +14,7 @@ public class DriverController {
     DriverService service;
 
     @PostMapping
-    public ResponseUtil addDriver(){
+    public ResponseUtil addDriver(@RequestParam String username, @RequestParam String password, @ModelAttribute DriverDTO dto){
         return new ResponseUtil("","","");
     }
 
