6cff2d77e983b3185f9557bd6738d5970f68a345
dimanranawaka <dimanranawaka@gmail.com>
2023-10-30 17:17:45 +0530

    Content Updated

--- Back_End/src/main/java/lk/ijse/spring/repo/RentRepo.java
+++ Back_End/src/main/java/lk/ijse/spring/repo/RentRepo.java
@@ -4,7 +4,10 @@ import lk.ijse.spring.entity.Rent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface RentRepo extends JpaRepository<Rent,String> {
     @Query(value = "SELECT rendId FROM Rent ORDER BY DESC LIMIT 1",nativeQuery = true)
     String generateLastRentId();
+    List<Rent> getRentByNic_Nic() throws RuntimeException;
 }
