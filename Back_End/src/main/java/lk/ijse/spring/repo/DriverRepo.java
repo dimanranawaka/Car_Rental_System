1529eefe737e14ca4fed2e8a6007241f1581e43a
dimanranawaka <dimanranawaka@gmail.com>
2023-10-30 14:42:32 +0530

    Content Updated

--- Back_End/src/main/java/lk/ijse/spring/repo/DriverRepo.java
+++ Back_End/src/main/java/lk/ijse/spring/repo/DriverRepo.java
@@ -12,4 +12,7 @@ public interface DriverRepo extends JpaRepository<Driver,String> {
 
     @Query(value = "SELECT * FROM Driver WHERE user_username=?",nativeQuery = true)
     Driver getDriverByUserName(String username);
+
+    @Query(value = "SELECT COUNT(nic) FROM Driver WHERE availabilityStatus='YES'",nativeQuery = true)
+    Long countAvailableDriverAmount() throws RuntimeException;
 }
---
+++ Back_End/src/test/java/lk/ijse/spring/repo/DriverRepoTest.java
@@ -0,0 +1,28 @@
+package lk.ijse.spring.repo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DriverRepoTest {
+
+    @Test
+    void getAllAvailableDriverList() {
+    }
+
+    @Test
+    void getDriverByUserName() {
+    }
+
+    @Test
+    void testGetAllAvailableDriverList() {
+    }
+
+    @Test
+    void testGetDriverByUserName() {
+    }
+
+    @Test
+    void countAvailableDriverAmount() {
+    }
+}
