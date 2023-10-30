60b156f95e8a01ecef7dadeb54a170283553984c
dimanranawaka <dimanranawaka@gmail.com>
2023-10-30 08:21:18 +0530

    Content Updated

--- Back_End/src/main/java/lk/ijse/spring/controller/DriverController.java
+++ Back_End/src/main/java/lk/ijse/spring/controller/DriverController.java
@@ -2,8 +2,10 @@ package lk.ijse.spring.controller;
 
 import lk.ijse.spring.service.DriverService;
 import lk.ijse.spring.service.RentService;
+import lk.ijse.spring.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class DriverController {
     @Autowired
     DriverService service;
-    @Autowired
-    RentService rentService;
+
+    @PostMapping
+    public ResponseUtil addDriver(){
+        return new ResponseUtil("","","");
+    }
+
 }
