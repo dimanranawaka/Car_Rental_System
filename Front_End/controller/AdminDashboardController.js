b287720b5c7e1d2ecfc4420254f77f7265f784f0
dimanranawaka <dimanranawaka@gmail.com>
2023-10-31 13:23:06 +0530

    Content Updated

---
+++ Front_End/controller/AdminDashboardController.js
@@ -0,0 +1,20 @@
+let regNum;
+let dailyMileage;
+let monthlyMileage;
+let dailyPrice;
+let monthlyPrice;
+let rentId;
+let currentUser;
+let customer;
+
+$.ajax({
+    url: baseUrl + "login",
+    method: "get",
+    async: false,
+    dataType: "json",
+    contentType: "application/json",
+    success: function (res) {
+        currentUser = res.data;
+        $("#user").text(res.data.username);
+    }
+});
