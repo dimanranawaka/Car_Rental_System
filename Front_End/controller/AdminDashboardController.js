22a3cbfce984bda23766fa4a78bdb611ad2ec7be
dimanranawaka <dimanranawaka@gmail.com>
2023-10-31 18:09:48 +0530

    Content Updated

--- Front_End/controller/AdminDashboardController.js
+++ Front_End/controller/AdminDashboardController.js
@@ -18,3 +18,19 @@ $.ajax({
         $("#user").text(res.data.username);
     }
 });
+
+homeLoader();
+
+function homeLoader(){
+    $("#home").fadeIn();
+    $("#home").attr("style","display : block !important");
+    $("#viewCustomer").attr("style","display : none !important");
+    $("#manageCustomers").attr("style","display : none !important");
+    $("#manageCar").attr("style","display : none !important");
+    $("#viewCar").attr("style","display : none !important");
+    $("#manageDriver").attr("style","display : none !important");
+    $("#drivers").attr("style","display : none !important");
+    $("#rents").attr("style","display : none !important");
+    $("#payments").attr("style","display : none !important");
+    $("#reports").attr("style","display : none !important");
+}
